6b802228a6d66f0787cc672941520139188ab9dc
Kyrre Havik Eriksen <kyrremann@gmail.com>
2017-07-03 13:23:55 +0200

    Added pymongo as required

--- setup.py
+++ setup.py
@@ -2,13 +2,11 @@ from setuptools import setup, find_packages
 
 setup(
     name='spotify_connect_scrobbler',
-    version='0.1',
+    version='0.2',
     license='MIT',
     packages=find_packages(),
-    install_requires=['click', 'python-dateutil', 'requests'],
+    install_requires=['click', 'python-dateutil', 'requests', 'pymongo'],
     entry_points={
-      'console_scripts': [
-          'scrobbler=spotify_connect_scrobbler.scrobbler:main',
-          'scrobbler-auth=spotify_connect_scrobbler.auth:main']
+      'console_scripts': ['scrobbler-auth=spotify_connect_scrobbler.auth:main']
     }
     )
